72f211294f93b51d52eb15495aa62ab62b60f085
EC2 Default User <ec2-user@ip-172-31-31-40.ec2.internal>
2021-10-02 22:11:30 +0000

    source-code created

---
+++ C_Program.c
@@ -0,0 +1,9 @@
+nitializationStatement; testExpression; updateStatement)
+{
+	    // statements inside the body of loop
+	    // }
+	    //
+	    //
+	    //
+	    //
+	    //
---
+++ HelloJava.java
@@ -0,0 +1,19 @@
+ass HelloJava {
+	public static void main(String args[]){
+		System.out.Println(“ I am your Java Program. Thank you! ”);
+	}
+}
+
+vi index.js
+{
+	  "name": "hello-world",
+		    "version": "1.0.0",
+		      "description": "",
+		        "main": "index.js",
+			  "scripts": {
+				      "test": "echo \"Error: no test specified\" && exit 1"
+					        },
+			    "author": "",
+			      "license": "ISC"
+}
+
